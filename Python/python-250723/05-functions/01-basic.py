cb0a56220938ba4fa6fbb0a3a60ad5cfaa133552
seorin <tjfls295@naver.com>
2025-07-23 11:44:43 +0900

    250723 python

---
+++ Python/python-250723/05-functions/01-basic.py
@@ -0,0 +1,20 @@
+# 함수 정의
+def make_sum(pram1, pram2):
+    """이것은 두 수를 받아
+     두 수의 합을 반환하는 함수입니다.
+     >>> make_sum(1, 2)
+    3
+    """
+    return pram1 + pram2
+
+
+# 함수 호출 및 반환 값 할당
+
+result = make_sum(100, 20)
+
+# print() 함수는 반환값이 없다.
+
+presult = print(result)  # 120
+
+print(presult)  # None
+
