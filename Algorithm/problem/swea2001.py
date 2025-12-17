43d3368628e33fa96d719640149041e0f461f8f0
seorin park <tjfls295@gmail.com>
2025-12-17 13:59:47 +0900

    251217

--- Algorithm/problem/swea2001.py
+++ Algorithm/problem/swea2001.py
@@ -1,25 +1,17 @@
 T = int(input())
 
-for testcase in range(1, T + 1):
+for tc in range(1, T+1):
     N, M = map(int, input().split())
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    
+    max_fly = 0
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+            fly = 0
+            for p in range(M):
+                for q in range(M):
+                    fly += graph[i+p][j+q]
 
-    main_list = [list(map(int, input().split())) for _ in range(N)]
+            max_fly = max(max_fly, fly)
 
-    # 1 3 3 6 7
-    # 8 13 9 12 8
-    # 4 16 11 12 6
-    # 2 4 1 23 2
-    # 9 13 4 7 3
-
-    max_sum = 0
-    # N = 5, M = 2
-    for i in range(N - M + 1):  # 0 - 4 [1, 0] [2, 0]
-        for j in range(N - M + 1):  # 0 - 4
-            sum = 0
-            for p in range(M):  # 0 - 1
-                for q in range(M):  # 0 -1
-                    sum += main_list[p + i][q + j]
-            if max_sum < sum:
-                max_sum = sum
-
-    print(f"#{testcase} {max_sum}")
+    print(f"#{tc} {max_fly}")
