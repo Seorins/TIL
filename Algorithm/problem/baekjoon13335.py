faccf70c853954e93d9836d972c308c334991d3f
seorin park <tjfls295@gmail.com>
2026-01-15 20:45:50 +0900

    260114

--- Algorithm/problem/baekjoon13335.py
+++ Algorithm/problem/baekjoon13335.py
@@ -3,18 +3,18 @@ from collections import deque
 n, w, L = map(int, input().split())
 trucks = list(map(int, input().split()))
 
-bridge = deque([0] * w)   # 다리 위 상태
+bridge = deque([0] * w) # 다리 위 상태
 time = 0
 idx = 0
-cur = 0  # 현재 다리 위 총 무게
+cur = 0 # 현재 다리 위 총 무게
 
 while idx < n or cur > 0:
     time += 1
 
-    out = bridge.popleft()   # 다리 끝에서 내려감
+    out = bridge.popleft() # 다리 끝에서 내려감
     cur -= out
 
-    # 다음 트럭 올릴 수 있으면 올리고 아니면 0(빈칸)
+    # 다음 트럭 올릴 수 있으면 올리고 아니면 0
     if idx < n and cur + trucks[idx] <= L:
         bridge.append(trucks[idx])
         cur += trucks[idx]
