c49415073a9d2bb63d84c7f8484cde34a3f9536c
박서린 <tjfls295@naver.com>
2026-01-26 22:30:53 +0900

    260126

---
+++ Algorithm/problem/baekjoon17413.py
@@ -0,0 +1,32 @@
+S = input()
+
+res = []
+stack = []
+in_tag = False
+
+for ch in S:
+    if ch == '<':
+        while stack:
+            res.append(stack.pop())
+        in_tag = True
+        res.append(ch)
+
+    elif ch == '>':
+        in_tag = False
+        res.append(ch)
+
+    elif in_tag:
+        res.append(ch)
+
+    else:
+        if ch == ' ':
+            while stack:
+                res.append(stack.pop())
+            res.append(' ')
+        else:
+            stack.append(ch)
+
+while stack:
+    res.append(stack.pop())
+
+print(''.join(res))
