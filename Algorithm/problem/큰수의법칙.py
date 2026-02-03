852bb65b3238f3e2e07eb95d75a02f965d4e74ea
박서린 <tjfls295@naver.com>
2026-02-03 21:05:07 +0900

    260203

---
+++ Algorithm/problem/큰수의법칙.py
@@ -0,0 +1,26 @@
+# 다양한 수로 이루어진 배열이 있을 때 M번 더하여 가장 큰 수 만들기
+# K = 동일한 인덱스 연속해서 더할 수 있는 개수 (수가 같아도 인덱스만 다르면 됨)
+
+N, M, K = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+numbers.sort() # 작은 수부터 정렬
+
+result = 0
+
+while True:
+
+    for i in range(K):
+        if M == 0:
+            break
+        result += numbers[N-1]
+        M -= 1
+
+    if M == 0:
+        break
+
+    result += numbers[N-2]
+    M -= 1
+    
+print(result)
+
