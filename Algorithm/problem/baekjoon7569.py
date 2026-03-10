d4e25cb489e3caceeb1408d0596170475c459fd4
박서린 <tjfls295@naver.com>
2026-03-10 22:20:07 +0900

    260310

--- Algorithm/problem/baekjoon7569.py
+++ Algorithm/problem/baekjoon7569.py
@@ -1,38 +1,43 @@
 from collections import deque
 
-m, n = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
+m, n, h = map(int, input().split())
+board = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
 q = deque()
 
-for i in range(n):
-    for j in range(m):
-        if board[i][j] == 1:
-            # 익은 토마토(1)의 좌표를 큐에 저장
-            q.append([i, j])
+# 처음부터 익은 토마토를 모두 큐에 넣기
+for z in range(h):
+    for x in range(n):
+        for y in range(m):
+            if board[z][x][y] == 1:
+                q.append((z, x, y))
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+# 6방향: 위, 아래, 앞, 뒤, 왼쪽, 오른쪽
+dz = [1, -1, 0, 0, 0, 0]
+dx = [0, 0, 1, -1, 0, 0]
+dy = [0, 0, 0, 0, 1, -1]
 
 while q:
-    x, y = q.popleft()
-    for i in range(4):
-        # 익은 토마토 상하좌우 돌면서 일수 저장
+    z, x, y = q.popleft()
+
+    for i in range(6):
+        nz = z + dz[i]
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < n and 0 <= ny < m:
-            if board[nx][ny] == 0:
-                board[nx][ny] = board[x][y] + 1
-                q.append([nx, ny])
+        if 0 <= nz < h and 0 <= nx < n and 0 <= ny < m:
+            if board[nz][nx][ny] == 0:
+                board[nz][nx][ny] = board[z][x][y] + 1
+                q.append((nz, nx, ny))
 
 result = 0
-for line in board:
-    for tomato in line:
-        if tomato == 0:
-            # 안익은 토마토(0)이 있으면 바로 정지
-            print(-1)
-            exit()
-    result = max(result, max(line))
-    
-# 1에서 시작했기 때문에 결과 값에서 1빼주기
-print(result-1)
+
+for box in board:
+    for row in box:
+        for tomato in row:
+            if tomato == 0:
+                print(-1)
+                exit()
+        result = max(result, max(row))
+
+# 처음 익은 토마토가 1부터 시작하므로 1 빼기
+print(result - 1)
