206e314f3dc2d7e018ccb90cc6a5441b9fc22ae9
Seorins <165753618+Seorins@users.noreply.github.com>
2026-04-25 05:39:04 +0900

    baekjoon9205.py 업데이트

--- Algorithm/problem/baekjoon9205.py
+++ Algorithm/problem/baekjoon9205.py
@@ -1,9 +1,3 @@
-# 맥주 한 박스에는 20병
-# 한 병으로 50m 이동
-# 최대 1000m 이동 가능
-# 편의점에 가서 다시 맥주를 채울 수 있음 
-# 결국 |x1 - x2| + |y1 - y2| 가 1000 이하이면 가능! 
-
 from collections import deque
 
 def bfs(places):
@@ -14,11 +8,9 @@ def bfs(places):
     while q:
         cur = q.popleft()
         x, y = places[cur]
-        
-        #  축제 좌표 
+    
         nx, ny = places[-1]
 
-        # 현재 위치 - 축제가 1000 이하이면 바로 happy
         if abs(x - nx) + abs(y - ny) <= 1000:
             return "happy"
 
