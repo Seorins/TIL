c9bc94acf76f4f7c914d1be042077788f4ff3070
seorin <tjfls295@gmail.com>
2026-07-28 12:22:44 +0900

    algorithm

--- Algorithm/problem/baekjoon2503.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-
-num, strike, ball = map(int, input().split())
-
-for a in range(1, 10):
-    for b in range(10):
-        for c in range(10):
-            if 
--- Algorithm/problem/swea25428.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-
-for tc in range(1, T+1):
-    N, K = map(int, input().split())
-    water = list(map(int, input().split()))
-
-    now = arrive = 0
-
-    
