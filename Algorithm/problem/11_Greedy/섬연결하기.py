144a532e1ff44be55fecb6cbffcf7f8a86115b12
seorin <tjfls295@gmail.com>
2026-09-04 14:09:02 +0900

    greedy

--- Algorithm/problem/11_Greedy/섬연결하기.py
+++ Algorithm/problem/11_Greedy/섬연결하기.py
@@ -1,3 +1,5 @@
+# kruskal
+
 def solution(n, costs):
 
     def find(x):
@@ -25,4 +27,40 @@ def solution(n, costs):
         if union(a, b):
             answer += cost
             
+    return answer
+
+
+# --------------------------------------------
+
+# prim
+
+import heapq
+
+def solution(n, costs):
+    
+    graph = [[] for _ in range(n)]
+    
+    for x, y, cost in costs: 
+        graph[x].append((cost, y))
+        graph[y].append((cost, x))
+        
+    visited = [False] * n
+    
+    heap = [(0, 0)]
+    
+    answer = 0
+    
+    while heap: 
+        cost, node = heapq.heappop(heap)
+        
+        if visited[node]: 
+            continue
+            
+        visited[node] = True
+        answer += cost
+        
+        for nxt_cost, nxt_node in graph[node]:
+            if not visited[nxt_node]:
+                heapq.heappush(heap, (nxt_cost, nxt_node))
+                
     return answer
