a8a1d9f19cd805a3be382ce6e6083376b41847ef
seorin <tjfls295@gmail.com>
2026-09-09 23:24:33 +0900

    sort

--- Algorithm/problem/21_Hash/전화번호목록.cpp
+++ Algorithm/problem/21_Hash/전화번호목록.cpp
@@ -22,3 +22,24 @@ bool solution(vector<string> phone_book) {
     
     return true;
 }
+
+
+// -------------------------------------------------------------------
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    
+    sort(phone_book.begin(), phone_book.end());
+        
+    for(int i = 0; i < phone_book.size()-1; i++){
+        if(phone_book[i] == phone_book[i+1].substr(0, phone_book[i].size())){
+            return false;
+        }
+    } return true;
+    
+}
